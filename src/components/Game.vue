a6010e4606bd7c61d2dd1754f3cbf75de1d189b4
Krystian Barchanski <krystian@appjobs.com>
2019-09-14 19:52:23 +0200

    indent fix

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,20 +3,20 @@
 </template>
 
 <script>
-import Phaser from 'phaser';
-import level from '../data/level';
-import Trash from '../game/trash';
-import Butt from '../game/butt';
-import EventBus from '../event-bus/event-bus';
+import Phaser from "phaser";
+import level from "../data/level";
+import Trash from "../game/trash";
+import Butt from "../game/butt";
+import EventBus from "../event-bus/event-bus";
 
-function preload () {
-    this.load.image('textures', 'textures.png');
-    this.load.image('trash', 'trash.png');
+function preload() {
+  this.load.image("textures", "textures.png");
+  this.load.image("trash", "trash.png");
 }
 
-function handleButtCollision (game, butt, target) {
-  if (target.label === 'trash') {
-    EventBus.$emit('win-game')
+function handleButtCollision(game, butt, target) {
+  if (target.label === "trash") {
+    EventBus.$emit("win-game");
   }
 }
 
@@ -24,32 +24,36 @@ function createTileMap(game) {
   const map = game.make.tilemap({
     data: level,
     tileWidth: 40,
-    tileHeight: 40,
+    tileHeight: 40
   });
-  const tiles = map.addTilesetImage('textures');
+  const tiles = map.addTilesetImage("textures");
   map.createStaticLayer(0, tiles, 0, 0);
 }
 
-function initMatter (game) {
-  // Draggable items 
+function initMatter(game) {
+  // Draggable items
   let canDrag = game.matter.world.nextGroup();
 
-  Trash(game)
-  Butt(game, canDrag)
+  Trash(game);
+  Butt(game, canDrag);
 
-  game.matter.add.mouseSpring({ length: 1, stiffness: 0.6, collisionFilter: { group: canDrag } });
+  game.matter.add.mouseSpring({
+    length: 1,
+    stiffness: 0.6,
+    collisionFilter: { group: canDrag }
+  });
 
-  game.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
-    let butt, target
-    if (bodyA.label === 'butt') {
-      butt = bodyA
-      target = bodyB
-    } else if (bodyB.label === 'butt') {
-      butt = bodyB
-      target = bodyA
+  game.matter.world.on("collisionstart", function(event, bodyA, bodyB) {
+    let butt, target;
+    if (bodyA.label === "butt") {
+      butt = bodyA;
+      target = bodyB;
+    } else if (bodyB.label === "butt") {
+      butt = bodyB;
+      target = bodyA;
     }
     if (butt) {
-      handleButtCollision(game, butt, target)
+      handleButtCollision(game, butt, target);
     }
   });
 }
@@ -58,17 +62,23 @@ function create() {
   this.matter.world.setBounds(0, 0, 800, 580);
   createTileMap(this);
   initMatter(this);
-    this.cannon = this.matter.add.sprite(200, 300, 'cannon', {
-        originX: 0,
+  this.cannon = this.matter.add
+    .sprite(200, 300, "cannon", {
+      originX: 0
     })
-      .setDisplaySize(60, 10)
-      .setOrigin(0, .5)
-      .setStatic(true)
+    .setDisplaySize(60, 10)
+    .setOrigin(0, 0.5)
+    .setStatic(true);
 }
 
-function update (time, delta) {
-    let {x,y} = this.input.activePointer;
-    this.cannon.rotation = Phaser.Math.Angle.Between(this.cannon.x, this.cannon.y, x, y);
+function update(time, delta) {
+  let { x, y } = this.input.activePointer;
+  this.cannon.rotation = Phaser.Math.Angle.Between(
+    this.cannon.x,
+    this.cannon.y,
+    x,
+    y
+  );
 }
 
 export default {
@@ -97,7 +107,7 @@ export default {
       scene: {
         preload: preload,
         create: create,
-        update: update,
+        update: update
       }
     });
   }
