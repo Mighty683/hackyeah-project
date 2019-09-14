bbf46213ea78b08818aef929100ba10f0c963155
Piotr Adamowicz <piotr.adamowicz@gmail.com>
2019-09-14 16:46:07 +0200

    Jumping letters

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div id="app">
-    <game />
+  <div id="app" @click="isIntro = false">
+    <intro v-if="isIntro"/>
+    <game v-else/>
   </div>
 </template>
 
 <script>
 import Game from './components/Game.vue'
+import Intro from './components/Intro';
 
 export default {
   name: 'app',
+
+  data () {
+    return {
+      isIntro: true,
+    };
+  },
+
   components: {
+    Intro,
     Game
   }
 }
---
+++ src/components/Intro.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="intro">
+    <div class="letter" v-for="(letter, index) in displayText" :key="index">
+      <div class="wave-big">
+        <div class="wave">
+          <div class="twiggle">{{ letter }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Intro',
+
+    data () {
+      return {
+        text: 'The text to be on the screen... and beyond!',
+        displayText: '',
+        currentLetter: 0,
+      };
+    },
+
+    methods: {
+      addLetter () {
+        this.displayText += this.text[this.currentLetter];
+        if (this.currentLetter < this.text.length - 1) {
+          this.currentLetter++;
+          this.renderText();
+        }
+      },
+
+      renderText () {
+        setTimeout(() => {
+          this.addLetter();
+        }, 100);
+      },
+    },
+
+    mounted () {
+      this.renderText();
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/styles/animations";
+
+  .intro {
+    font-family: monospace;
+    font-weight: bold;
+    font-size: 8vw;
+    white-space: nowrap;
+
+    .letter {
+      display: inline-block;
+      width: 6vw;
+
+    }
+  }
+
+</style>
