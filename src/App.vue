b783b9bef5b9c6c44336de0ce8fe20f4ac348239
sylwester.denko <sylwester.denko@sollers.eu>
2019-09-14 16:44:28 +0200

    -added modal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
+    <button type="button" class="btn-close" @click="toggleModal">ELO 320</button>
     <game />
+    <modal @close="toggleModal" v-if="isModalVisible" />
   </div>
 </template>
 
 <script>
-import Game from './components/Game.vue'
-
+import Game from "./components/Game.vue";
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Game
+    Game,
+    Modal
+  },
+  data() {
+    return {
+      isModalVisible: false
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.isModalVisible = !this.isModalVisible;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
