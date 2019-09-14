1b2a423761a144ba0538fd3b96f42cf4dff2aa1f
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-09-14 17:50:16 +0200

    Clean Game vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,18 +8,25 @@ import level from '../data/level'
 function preload () {
     this.load.image('textures', 'textures.png');
 }
+function createTileMap () {
+  const map = this.make.tilemap({
+    data: level,
+    tileWidth: 40,
+    tileHeight: 40,
+  })
+  const tiles = map.addTilesetImage('textures')
+  map.createStaticLayer(0, tiles, 0, 0);
+}
 
-function create ()
-{
-    this.matter.world.setBounds();
+function initMatter () {
 
-    const map = this.make.tilemap({
-      data: level,
-      tileWidth: 40,
-      tileHeight: 40,
-    })
-    const tiles = map.addTilesetImage('textures')
-    map.createStaticLayer(0, tiles, 0, 0);
+}
+
+function create () {
+    this.matter.world.setBounds(0, 0, 800, 580);
+    createTileMap();
+    initMatter();
+
 }
 
 export default {
