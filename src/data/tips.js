2f613457f2009c4edc6bcab3dcd501c714fa83a6
sylwester.denko <sylwester.denko@sollers.eu>
2019-09-15 06:40:52 +0200

    -tip

--- src/data/tips.js
+++ src/data/tips.js
@@ -4,7 +4,8 @@ const tips = [
     "Weight of cigarettes littered annually up to 800 000 t",
     "Numbers of cigarette sold globally annually : 5,6 trillions",
     "Tobacco value of agri-food exports – 11% ( Higher was recorded only in trade in meat and meat products )",
-    "Every fifth waste found on the Baltic beack is a butt"
+    "Every fifth waste found on the Baltic beack is a butt",
+    "One but can cost you even 2 Bilion PLN ? In 2008 one guy from Poznan lead to burn estate by throwing butt"
 ]
 
 
