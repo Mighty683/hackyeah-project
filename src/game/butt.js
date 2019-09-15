ba7319d9defb914d48bf4ce807fccd3a4fd115cf
Krystian Barchanski <krystian@appjobs.com>
2019-09-15 02:10:59 +0200

    fire the butt

--- src/game/butt.js
+++ src/game/butt.js
@@ -1,10 +1,10 @@
 import Phaser from 'phaser';
 let Bodies = Phaser.Physics.Matter.Matter.Bodies;
 
-export default (game, { collisionGroup }) => game.matter.add
+export default (game, { collisionGroup, x = 300, y = 300 }) => game.matter.add
   .image(0, 0, 'butt')
   .setExistingBody(Bodies.rectangle(0, 0, 30, 30, {
     label: 'dupa'
   }))
   .setCollisionGroup(collisionGroup)
-  .setPosition(300, 300);
+  .setPosition(x, y);
