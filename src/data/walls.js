607ec17b9fc4d448638bf00c82b7cac713af6fdf
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-09-15 03:34:08 +0200

    Set more walls

--- src/data/walls.js
+++ src/data/walls.js
@@ -1,18 +1,34 @@
 const walls = [
     {
-        height: 80,
-        rotation: 45,
+        height: 200,
+        rotation: 0,
         position: {
-            x: 100,
+            x: 200,
+            y: 480
+        }
+    },
+    {
+        height: 350,
+        rotation: 0,
+        position: {
+            x: 350,
             y: 200
         }
     },
     {
-        height: 140,
-        rotation: 60,
+        height: 80,
+        rotation: 0,
         position: {
-            x: 500,
-            y: 300
+            x: 450,
+            y: 540
+        }
+    },
+    {
+        height: 200,
+        rotation: 90,
+        position: {
+            x: 600,
+            y: 400
         }
     }
 ]
