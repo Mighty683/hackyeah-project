855dee75e4046fe78336ba59410e5c8b63bf0407
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-09-15 03:25:27 +0200

    Thrash fix

--- src/game/trash.js
+++ src/game/trash.js
@@ -1,10 +1,17 @@
 import Phaser from 'phaser';
 let Bodies = Phaser.Physics.Matter.Matter.Bodies;
 
-export default (game) => game.matter.add
+export default (game) => {
+  let trashSensor = Bodies.rectangle(0, 0, 40, 65, { isSensor: true, label: 'trash' })
+  let trashBody = Bodies.rectangle(0, 0, 60, 60)
+  let compoundBody = Phaser.Physics.Matter.Matter.Body.create({
+      parts: [ trashBody, trashSensor, ],
+      inertia: Infinity
+  });
+
+  return game.matter.add
   .image(0, 0, 'trash')
-  .setExistingBody(Bodies.rectangle(0, 0, 40, 80, {
-    label: 'trash'
-  }))
+  .setExistingBody(compoundBody)
   .setPosition(740, 540)
   .setStatic(true);
+}
